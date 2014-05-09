50ec6a401c71d80b8ba1cafbe906026dd7d322e4
LI Yu <liyu@clustertech.com>
2014-05-09 15:38:03 +0800

    script for init the iso workdir.

--- devtool/iso/geniso.sh
+++ devtool/iso/geniso.sh
@@ -1,4 +1,8 @@
+VER_SMARTOS=20140404T001635Z
+VER_FIFO=0.4.4
+
 cd amd64
 scp root@192.168.56.10:/opt/dogeos/boot_archive* .
 cd -
-LC_ALL=C mkisofs -R -b boot/grub/stage2_eltorito --follow-links -no-emul-boot -boot-load-size 4 -boot-info-table -quiet -o dogeos-test.iso dogeos-test/
+
+LC_ALL=C mkisofs -R -b boot/grub/stage2_eltorito --follow-links -no-emul-boot -boot-load-size 4 -boot-info-table -quiet -o dogeos-${VER_SMARTOS}-${VER_FIFO}.iso cdrom/
---
+++ devtool/iso/init.sh
@@ -0,0 +1,15 @@
+# usage:
+#   init.sh <target_dir>
+
+MYDIR=`pwd`
+cd $1
+
+mkdir cdrom
+mkdir dogeos-extra
+
+cp -v ${MYDIR}/geniso.sh .
+
+ln -fs ./cdrom/platform/i86pc/amd64 amd64
+ln -fs ../dogeos-extra/dogeos cdrom/dogeos
+
+cd -
