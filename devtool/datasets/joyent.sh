65c26e8bae28068c37fe409c141b68979565595c
LI Yu <liyu@clustertech.com>
2014-05-16 20:30:23 +0800

    fix some urls of datasets file (joyent.sh), as well as some fixes in build.sh

--- devtool/datasets/joyent.sh
+++ devtool/datasets/joyent.sh
@@ -1,16 +1,18 @@
+# with no special instructions, all datasets are 64bit
+
 getdatasets()
 {
   NAME=$1
   UUID=$2
   wget https://images.joyent.com/images/${UUID} -O ${NAME}.dsmanifest
-  wget https://images.joyent.com/images/${UUID}/file -O ${NAME}.gz
+  wget -c https://images.joyent.com/images/${UUID}/file -O ${NAME}.gz
 }
 
 # http://wiki.joyent.com/wiki/display/jpc2/CentOS#CentOS-2.6.1
 getdatasets CentOS-2.6.1 19daa264-c4c4-11e3-bec3-c30e2c0d4ec0
 
-# http://wiki.joyent.com/wiki/display/jpc2/Ubuntu+Certified#UbuntuCertified-14.0420140416
-getdatasets UbuntuCertified-14.0420140416 df165dad-0a5d-4eb7-8646-50507ff1c136
+# http://wiki.joyent.com/wiki/display/jpc2/Ubuntu#Ubuntu-2.4.2
+getdatasets Ubuntu-2.4.2 d2ba0f30-bbe8-11e2-a9a2-6bc116856d85
 
 # http://wiki.joyent.com/wiki/display/jpc2/SmartMachine+Base#SmartMachineBase-14.1.0
 getdatasets SmartMachineBase-14.1.0 8639203c-d515-11e3-9571-5bf3a74f354f
