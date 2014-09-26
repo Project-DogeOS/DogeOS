09c2317b50d412444ae80c442e7e9c12d9959ae1
LI Yu <liyu@clustertech.com>
2014-09-26 13:54:12 +0800

    improved the pkg.js tool to be able to lock down versions of both fifo and joyent

---
+++ devtool/extra/gen_fifo_pkg_summary.sh
@@ -0,0 +1,6 @@
+# $1 is the filelist file
+# $2 is the category name
+#   e.g., ../extra/gen_fifo_pkg_summary.sh ../filelist/fifo-filelist-0.6.0.txt fifo
+for f in `cat $1`; do
+  node pkg.js $2/pkg_summary pkg_summary $(basename $f);
+done
