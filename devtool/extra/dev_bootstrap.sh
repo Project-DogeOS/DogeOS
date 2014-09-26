97f4ef94c6d07164ff127e41e489abba7429cda6
LI Yu <liyu@clustertech.com>
2014-09-26 10:31:18 +0800

    fixed the missing files in filelist, and update to newest fifo pkgs

--- devtool/extra/dev_bootstrap.sh
+++ devtool/extra/dev_bootstrap.sh
@@ -8,8 +8,7 @@ confirm () {
       true
       ;;
     *)
-      #false
-      exit 100 # exit as user wish
+      false
       ;;
   esac
 }
@@ -19,22 +18,34 @@ cp $(readlink -e $(dirname $0))/pkg.js .
 
 # download joyent pkg_summary
 confirm "Download joyent pkg_summary ?"
-JOYENT_VER=2014Q2
-mkdir -p joyent; cd joyent
-  wget http://pkgsrc.joyent.com/packages/SmartOS/${JOYENT_VER}/x86_64/All/pkg_summary.bz2
-  bunzip2 pkg_summary.bz2
-cd -
+if [[ $? -eq 0 ]]; then
+  JOYENT_VER=2014Q2
+  mkdir -p joyent; cd joyent
+    wget http://pkgsrc.joyent.com/packages/SmartOS/${JOYENT_VER}/x86_64/All/pkg_summary.bz2
+    bunzip2 pkg_summary.bz2
+  cd -
+else
+  echo "skipped"
+fi
 
 # download fifo pkg_summary
 confirm "Download fifo pkg_summary ?"
-mkdir -p fifo; cd fifo
-  wget http://release.project-fifo.net/pkg/rel/pkg_summary.bz2
-  bunzip2 pkg_summary.bz2
-cd -
+if [[ $? -eq 0 ]]; then
+  mkdir -p fifo; cd fifo
+    wget http://release.project-fifo.net/pkg/rel/pkg_summary.bz2
+    bunzip2 pkg_summary.bz2
+  cd -
+else
+  echo "skipped"
+fi
 
 # download latest chunter
 confirm "Download latest chunter ?"
-mkdir -p chunter; cd chunter
-  curl -O http://release.project-fifo.net/chunter/rel/chunter-latest.gz
-  curl -O http://release.project-fifo.net/chunter/rel/chunter.version
-cd -
+if [[ $? -eq 0 ]]; then
+  mkdir -p chunter; cd chunter
+    curl -O http://release.project-fifo.net/chunter/rel/chunter.version
+    curl -O http://release.project-fifo.net/chunter/rel/chunter-latest.gz
+  cd -
+else
+  echo "skipped"
+fi
